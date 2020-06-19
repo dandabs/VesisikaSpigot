859b710177fc440bac3adaf6d36dce335ce834ef
Daniel Adams <danstanswinxp@gmail.com>
2020-06-19 20:05:47 +0300

    Initial commit

---
+++ src/main/java/app/vesisika/Plugin.java
@@ -0,0 +1,16 @@
+package app.vesisika;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable is called!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("onDisable is called!");
+    }
+
+}
